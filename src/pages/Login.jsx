35d7bfb52ce3b64c80f57e9ab116393adc429b94
park-ria <danny6217@gmail.com>
2024-10-10 16:52:56 +0900

    first commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Form, Button, Container } from "react-bootstrap";
+
+const Login = ({ setAuthenticate }) => {
+  const navigate = useNavigate();
+  const loginUser = (e) => {
+    e.preventDefault();
+    setAuthenticate(true);
+    navigate("/");
+  };
+
+  return (
+    <Container>
+      <Form onSubmit={loginUser}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="danger" type="submit">
+          Login
+        </Button>
+      </Form>
+    </Container>
+  );
+};
+
+export default Login;
---
+++ src/pages/ProductAll.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ProductCard from "../components/ProductCard";
+import { Col, Container, Row } from "react-bootstrap";
+
+const ProductAll = () => {
+  const [productList, setProductList] = useState([]);
+  const [query, setQuery] = useSearchParams();
+
+  const getProducts = async () => {
+    const searchQuery = query.get("q") || "";
+    const url = `http://localhost:3000/products?q=${searchQuery}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    setProductList(data);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, [query]);
+
+  return (
+    <Container>
+      <Row>
+        {productList.map((menu, index) => (
+          <Col lg={3} key={index}>
+            <ProductCard item={menu} />
+          </Col>
+        ))}
+        ;
+      </Row>
+    </Container>
+  );
+};
+
+export default ProductAll;
